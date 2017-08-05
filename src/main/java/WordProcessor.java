fac9c956312730c8f03ea0cfd498539225c1b179
lskoff <lucasskoff@gmail.com>
2017-08-05 14:31:18 -0500

    Created hash map with first letter of each word as key and empty list of points as value.

--- src/main/java/WordProcessor.java
+++ src/main/java/WordProcessor.java
@@ -1,13 +1,32 @@
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 /**
  * A utility class to parse comma separated words and store data in approriate data structures.
  */
-public class WordProcessor
+class WordProcessor
 {
-    public List<String> parseStringAsArrayList(String inputString){
+    /**
+     * Creates a hashmap where key is first letter of each word in the word list and
+     * value is an empty List of points to be later filled with the position of each letter in grid.
+     * @return HashMap where Character -> empty List<Point>
+     */
+    HashMap<Character, List<Point>> createHashMapOfWordFirstLetters(List<String> wordList){
+        HashMap<Character, List<Point>> firstLetterPositions = new HashMap<>();
+        for(String word : wordList){
+            firstLetterPositions.put(word.charAt(0), new ArrayList<>());
+        }
+        return firstLetterPositions;
+    }
+
+    /**
+     * Parses a string of comma separated words into an ArrayList<String>
+     * @return ArrayList<String> containing each word
+     */
+    List<String> parseStringAsArrayList(String inputString){
         Scanner scanner = new Scanner(inputString);
         scanner.useDelimiter(",");
         List<String> wordList = new ArrayList<>();
--- src/test/java/WordProcessorTest.java
+++ src/test/java/WordProcessorTest.java
@@ -3,16 +3,20 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.apache.commons.text.RandomStringGenerator;
+
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class WordProcessorTest
 {
-    WordProcessor wordProcessor;
-    List<String> wordList;
-    RandomStringGenerator randomStringGenerator;
-    Random rg;
+    private WordProcessor wordProcessor;
+    private List<String> wordList;
+    private RandomStringGenerator randomStringGenerator;
+    private Random rg;
+    private String firstInputWord;
+    private String secondInputWord;
 
     @Before
     public void init(){
@@ -20,23 +24,25 @@ public class WordProcessorTest
         wordList = new ArrayList<>();
         randomStringGenerator = new RandomStringGenerator.Builder().withinRange('a', 'z').build();
         rg = new Random();
+        firstInputWord = "Bones";
+        secondInputWord = "Spock";
     }
 
     @Test
     public void ReadSingleWordFromStringAndStoreAsList(){
-        wordList.add("Bones");
+        wordList.add(firstInputWord);
         assertEquals(wordList, wordProcessor.parseStringAsArrayList("Bones"));
     }
 
     @Test
     public void ReadTwoWordsFromStringSeparatedByCommaAndStoreAsList(){
-        wordList.add("Bones");
-        wordList.add("Spock");
+        wordList.add(firstInputWord);
+        wordList.add(secondInputWord);
         assertEquals(wordList, wordProcessor.parseStringAsArrayList("Bones,Spock"));
     }
 
     @Test
-    public void ReadAnyNumberOfWordsFromStringSeperatedByCommasAndStoreAsList(){
+    public void ReadAnyNumberOfWordsFromStringSeparatedByCommasAndStoreAsList(){
         int numberOfWordsToRead = rg.nextInt(12);
         String inputLine = StringUtils.EMPTY;
         for(int i = 0; i < numberOfWordsToRead; i++){
@@ -46,5 +52,12 @@ public class WordProcessorTest
         }
 
         assertEquals(wordList, wordProcessor.parseStringAsArrayList(inputLine));
+    }
+
+    @Test
+    public void CreateHashMapWithCorrectNumberOfKeysFromWordListNoDuplicateWords(){
+        wordList.add(firstInputWord);
+        wordList.add(secondInputWord);
+        assertEquals(wordList.size(), wordProcessor.createHashMapOfWordFirstLetters(wordList).size());
     }
 }
