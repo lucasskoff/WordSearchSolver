5abc2009628cc991e1da0cf598243337322d0455
lskoff <lucasskoff@gmail.com>
2017-08-05 13:49:27 -0500

    WordProcessor can read in any number of words from input String.

--- src/test/java/WordProcessorTest.java
+++ src/test/java/WordProcessorTest.java
@@ -1,13 +1,17 @@
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.apache.commons.text.RandomStringGenerator;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class WordProcessorTest
 {
     WordProcessor wordProcessor;
     List<String> wordList;
+
     @Before
     public void init(){
         wordProcessor = new WordProcessor();
@@ -25,5 +29,20 @@ public class WordProcessorTest
         wordList.add("Bones");
         wordList.add("Spock");
         assertEquals(wordList, wordProcessor.parseStringAsArrayList("Bones,Spock"));
+    }
+
+    @Test
+    public void ReadAnyNumberOfWordsFromStringSeperatedByCommasAndStoreAsList(){
+        Random rg = new Random();
+        int numberOfWordsToRead = rg.nextInt(12);
+        RandomStringGenerator randomStringGenerator = new RandomStringGenerator.Builder().withinRange('a', 'z').build();
+        String inputLine = StringUtils.EMPTY;
+        for(int i = 0; i < numberOfWordsToRead; i++){
+            String randomWord = randomStringGenerator.generate(5);
+            inputLine = inputLine.concat(randomWord + ",");
+            wordList.add(randomWord);
+        }
+
+        assertEquals(wordList, wordProcessor.parseStringAsArrayList(inputLine));
     }
 }
