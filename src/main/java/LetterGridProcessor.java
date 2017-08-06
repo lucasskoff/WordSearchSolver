3999a4bccd651cb89f7c27d4bf1cae2a5ab3fcda
lskoff <lucasskoff@gmail.com>
2017-08-06 11:53:15 -0500

    GridProcessor creates correct number of rows and columns for a 3x3 entry.

--- src/main/java/LetterGridProcessor.java
+++ src/main/java/LetterGridProcessor.java
@@ -1,4 +1,21 @@
-public class LetterGridProcessor
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class LetterGridProcessor
 {
 
+	static char[][] parseGridFromFileAsCharArray(File file) {
+		try {
+			Scanner scanner = new Scanner(file);
+			scanner.nextLine(); //Throw away top line as it is only words.
+			String currentLine = scanner.nextLine();
+			int widthHeight = (currentLine.length() / 2); //Removing commas from count.
+			char[][] letterGrid = new char[widthHeight][widthHeight];
+			return letterGrid;
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
--- src/test/java/LetterGridProcessorTest.java
+++ src/test/java/LetterGridProcessorTest.java
@@ -1,7 +1,27 @@
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.File;
 
 public class LetterGridProcessorTest
 {
-	//@Test
+	private File threeByThreeInput;
+	private char[][] threeByThreeInputSolutionArray = {{'D','O','G'},{'C','A','T'},{'R','A','T'}};
+
+	@Before
+	public void Init(){
+		threeByThreeInput = new File("resources/horizontalWordsThreeByThreeGrid");
+	}
+
+	@Test
+	public void ThreeByThreeGridFromFileIsCorrectSize(){
+		try {
+			assertEquals(threeByThreeInputSolutionArray.length, LetterGridProcessor.parseGridFromFileAsCharArray(threeByThreeInput).length);
+		}catch(NullPointerException e){
+			Assert.fail("NullPointerException thrown. FileNotFound in LetterGridProcessor.parseGridFromFileAsCharArray.");
+		}
+	}
 
 }
