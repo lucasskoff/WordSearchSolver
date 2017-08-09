d747c37c060c5dce1883aae2940c9e389a795e20
lskoff <lucasskoff@gmail.com>
2017-08-08 19:35:50 -0500

    Was able to find "DOG" when in vertical direction!

--- src/test/java/WordSearchSolverTest.java
+++ src/test/java/WordSearchSolverTest.java
@@ -14,16 +14,33 @@ public class WordSearchSolverTest
 {
 	char[][] twoBytwoGrid = {{'D','O'},{'C','A'}};
 	private char[][] threeByThreeSolutionArray = {{'D','O','G'},{'C','A','T'},{'R','A','T'}};
+	private char[][] threeByThreeVerticalSolutionArray = {{'D', 'C', 'R'}, {'O', 'A', 'A'}, {'G', 'T', 'T'}};
+	List<Point> pointsToTest;
+	@Before
+	public void init(){
+		pointsToTest = new ArrayList<>();
+	}
 	@Test
-	public void ReturnPointsOfHorizontalWordDogInSimpleThreeByThree(){
-		Point[] correctPointsDogArray = {new Point(0,0), new Point(1,0), new Point(2,0)};
-		List<Point> correctPointsList = Arrays.asList(correctPointsDogArray);
-		List<Point> pointsToTest = new ArrayList<Point>();
+	public void ReturnPointsOfHorizontalWordDogInSimpleThreeByThreeGrid(){
+		Point[] correctPointsHorizontalArray = {new Point(0,0), new Point(1,0), new Point(2,0)};
+		List<Point> correctPointsList = Arrays.asList(correctPointsHorizontalArray);
 		if(WordSearchSolver.wordFinder(Direction.Horizontal, new Point(0,0), "Dog", threeByThreeSolutionArray, pointsToTest))
 		{
 			assertEquals(correctPointsList, pointsToTest);
 		}else {
 			Assert.fail("Incapable of finding word in test ReturnPointsOfHorizontalWordDogInSimpleThreeByThree.");
+		}
+	}
+
+	@Test
+	public void ReturnPointsOfVerticalWordDogInSimpleThreeByThreeGrid()
+	{
+		Point[] correctPointsVerticalArray = {new Point(0,0), new Point(0, 1), new Point(0, 2)};
+		List<Point> correctPointsList = Arrays.asList(correctPointsVerticalArray);
+		if(WordSearchSolver.wordFinder(Direction.Vertical, new Point(0,0), "Dog", threeByThreeSolutionArray, pointsToTest)){
+			assertEquals(correctPointsList, pointsToTest);
+		}else {
+			Assert.fail("Incapable of finding word in test ReturnPointsOfVerticalWordDogInSimpleThreeByThreeGrid.");
 		}
 	}
 
